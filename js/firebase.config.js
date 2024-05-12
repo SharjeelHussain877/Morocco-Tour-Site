803412143396ac465cf265d836c6ba040f373aa0
Sharjeel Hussain <133387058+SharjeelHussain877@users.noreply.github.com>
2024-05-12 21:26:53 +0500

    removed my api keys.config.js

--- js/firebase.config.js
+++ js/firebase.config.js
@@ -1,22 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
-const firebaseConfig = {
-  apiKey: "AIzaSyDwCFpXKuevlfJYXZ63sIPMCQV5IKZSqFQ",
-  authDomain: "morocco-tour-811ca.firebaseapp.com",
-  projectId: "morocco-tour-811ca",
-  storageBucket: "morocco-tour-811ca.appspot.com",
-  messagingSenderId: "657159408119",
-  appId: "1:657159408119:web:3ce3f2d7d2d3cfc1fcc6f2",
-  measurementId: "G-QT2QCQNGKP"
-};
-// apiKey: "AIzaSyDKNk73GDUFiH4F4P79wwRPRDCKC-ZvomE",
-// authDomain: "tour-c8fe3.firebaseapp.com",
-// projectId: "tour-c8fe3",
-// storageBucket: "tour-c8fe3.appspot.com",
-// messagingSenderId: "273054360586",
-// appId: "1:273054360586:web:b3f66e9ab067815c4febf3",
-// measurementId: "G-NJCFHW5XPW"
 
+const firebaseConfig = {
+  apiKey: "your api key",
+  authDomain: "your auth domain",
+  projectId: "your project id",
+  storageBucket: "your storage bucket,
+  messagingSenderId: "messsaging sender if",
+  appId: "app id",
+  measurementId: "measurement id"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
